5b4f804abe1f8d4f5399e041b587c881323fa8ca
Sylvester Obeng <122728594+devxprogramming@users.noreply.github.com>
2025-11-26 15:23:49 +0000

    Secure command API and improve model metadata

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,13 @@
-from django.urls import path, include
-from .views import *
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+
+from .views import CommandAPI
 
 app_name = 'api'
 
 router = DefaultRouter()
-
-router.register(r'commands', CommanAPI, basename='commands_api')
-
+router.register(r'commands', CommandAPI, basename='commands_api')
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
 ]
